d3ae327bdaa86dedfcc1ad458e1e6676d1d84b7c
ndmbachia@outlook.com <ndmbachia@outlook.com>
2024-02-22 18:07:43 +0300

    index updated

---
+++ migrations/versions/d759559f2e8c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d759559f2e8c
+Revises: 
+Create Date: 2024-02-20 19:33:58.902111
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd759559f2e8c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
